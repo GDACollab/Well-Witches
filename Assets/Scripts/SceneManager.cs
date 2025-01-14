009c07a674ed66d7d7cedf93a084e83dbab317e1
Brian Feng <brainfeng135@gmail.com>
2025-01-13 16:49:51 -0800

    added singleton framework

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager Instance {get; private set;}
+    
+    private void Awake(){
+        if(Instance != null && Instance != this){
+            Destroy(this);
+        }
+        else{
+            Instance = this;
+        }
+    }
+}
