53640f82783a7a10d893fa710c3131fc3861d97f
Justin Fong <justinvf2018@gmail.com>
2025-01-24 13:59:39 -0800

    basic stat changes script

---
+++ Assets/Scripts/GoodEvent.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GoodEvent : MonoBehaviour
+{
+   public enum Buffs {healthUp, attackUp};
+   //public enum Debuffs {healthDown, attackDown};
+
+   public float buffAmount;
+
+   private WardenStats warden;
+
+   public Buffs buff;
+   //public Debuffs debuff;
+
+   private void Awake(){
+      warden = FindObjectOfType<WardenStats>();
+   }
+
+   private void OnTriggerEnter2D(Collider2D collision){
+      if(collision.gameObject.tag == "Player"){
+         switch (buff)
+         {
+            case Buffs.healthUp:
+               warden.health += buffAmount;
+               Debug.Log(warden.health);
+               break;
+            case Buffs.attackUp:
+               warden.attackPower += buffAmount;
+               Debug.Log(warden.attackPower);
+               break;
+            default:
+               break;
+         }
+      }
+
+        Destroy(gameObject);
+   }
+
+}
