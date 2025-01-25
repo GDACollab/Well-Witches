866a1a2684fececfde58ceb1bf8bac96cd5eef3b
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-01-25 00:44:57 -0800

    funny shader/post processing stuff that won't get added

--- Assets/Scripts/Ultimate.cs
+++ Assets/Scripts/Ultimate.cs
@@ -1,10 +1,24 @@
+using System.Collections;
+using Unity.Properties;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Ultimate : MonoBehaviour
 {
     [SerializeField] private ParticleSystem chargingParticles;
     [SerializeField] private ParticleSystem orb;
     [SerializeField] private ParticleSystem shockwave;
+
+    public CameraShake cameraShake;
+    public GameObject volume;
+    public GameObject light2d;
+
+    private void Start()
+    {
+        volume.SetActive(false);
+        light2d.SetActive(false);
+    }
 
     private void Update()
     {
@@ -13,6 +27,7 @@ public class Ultimate : MonoBehaviour
             chargingParticles.Play();
             orb.Play();
             shockwave.Play();
+            StartCoroutine(cameraShake.Shake(.15f, .4f, 1f, volume, light2d));
         }
     }
 }
