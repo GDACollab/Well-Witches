b88a0822bc0e6163d69fd2e39004af1955e1580b
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-01-28 10:08:25 -0800

    impact testing

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -38,6 +38,12 @@ public class Bullet : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+
 
     IEnumerator Despawn()
     {
