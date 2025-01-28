00d4b63f1b737410f0ad250909eb02d9c42ef10e
melikestuff <149332214+melikestuff@users.noreply.github.com>
2025-01-27 23:35:13 -0800

    Merged with procedural gen branch
    Added perlinnoisemap generator
    Merged ashton's perlinnoise branch

---
+++ Assets/Scripts/PerlinNoise/PerlinNoiseGenerator.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perlinNoiseMap : MonoBehaviour
+{
+
+    //2 diff dictionaries to reference tiles/tileGroups
+    Dictionary<int, GameObject> tileset;
+    Dictionary<int, GameObject> tile_groups;
+
+
+    //4 different tiles
+    public GameObject TileDirt;
+    public GameObject TileGrass;
+    public GameObject TileRock;
+    public GameObject TileGlass;
+
+    //size of map
+    int mapWidth = 160;
+    int mapHeight = 90;
+
+
+    //2D Lists to hold tiles and frequency
+    List<List<int>> noiseGrid = new List<List<int>>();
+    List<List<GameObject>> tileGrid = new List<List<GameObject>>();
+
+    //var to magnify randomness/customization of perlin noise
+    //any value between 4 and 20
+    float magnification = 14.0f;
+
+    int xOffset = -10; //if increase/decrease, move right/left
+    int yOffset = -10; //if inc/dec, move up/down
+    // Start is called before the first frame update
+    void Start()
+    {
+        createTileset();
+        createTileGroups();
+        generateMap();
+
+    }
+
+    void createTileset()
+    {
+        tileset = new Dictionary<int, GameObject>();
+        tileset.Add(0, TileDirt);
+        tileset.Add(1, TileGrass);
+        tileset.Add(2, TileRock);
+        tileset.Add(3, TileGlass);
+    }
+
+    void createTileGroups()
+    {
+        tile_groups = new Dictionary<int, GameObject>();
+
+        foreach (KeyValuePair<int, GameObject> prefab_pair in tileset)
+        {
+
+            GameObject tile_group = new GameObject(prefab_pair.Value.name);
+            tile_group.transform.parent = gameObject.transform;
+            tile_group.transform.localPosition = new Vector3(0, 0, 0);
+            tile_groups.Add(prefab_pair.Key, tile_group);
+
+        }
+    }
+
+    void generateMap()
+    {
+        for (int x = 0; x < mapWidth; x++)
+        {
+
+            noiseGrid.Add(new List<int>());
+            tileGrid.Add(new List<GameObject>());
+
+            for (int y = 0; y < mapHeight; y++)
+            {
+                int tile_id = getIdUsingPerlin(x, y);
+                noiseGrid[x].Add(tile_id);
+                createTile(tile_id, x, y);
+            }
+
+        }
+    }
+
+    int getIdUsingPerlin(int x, int y)
+    {
+        //will generate a bunch of random numbers
+        float rawPerlin = Mathf.PerlinNoise((x - xOffset) / magnification, (y - yOffset) / magnification);
+
+        //will clamp numbers between 0 and 1, then multiplied by count(4) so that we can assign them to diff tiles
+        float clamp_perlin = Mathf.Clamp(rawPerlin, 0, 1);
+        float scaledPerlin = clamp_perlin * tileset.Count;
+
+        if (scaledPerlin == 4)
+        {
+            scaledPerlin = 3;
+        }
+
+        return Mathf.FloorToInt(scaledPerlin);
+    }
+
+    void createTile(int tile_id, int x, int y)
+    {
+        GameObject tilePrefab = tileset[tile_id];
+        GameObject tileGroup = tile_groups[tile_id];
+
+        GameObject tile = Instantiate(tilePrefab, tileGroup.transform);
+
+        tile.name = string.Format("tile_x{0}_y{1}", x, y);
+        tile.transform.localPosition = new Vector3(x, y, 0);
+
+        tileGrid[x].Add(tile);
+    }
+
+
+}
