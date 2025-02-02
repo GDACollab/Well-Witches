ad5a5ed3c7a64f5fb45aa49ddf3e2967cd3c6b0c
bagelcat11 <lynn.g0032@gmail.com>
2025-02-02 00:15:39 -0800

    pause scene implemented into hub with test asset; BacktoHub button only shown when pausing from gameplay

--- Assets/Scripts/Scenes/PauseManager.cs
+++ Assets/Scripts/Scenes/PauseManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 public class PauseManager : MonoBehaviour
 {
     [SerializeField] public GameObject pauseScene;
+    [SerializeField] public GameObject hubButton;
 
     private void Awake()
     {
@@ -17,6 +18,16 @@ public class PauseManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            // not 100% sure what the pause implementation we want will be,
+            // but for now we'll just go with manipulating timeScale
+
+            // make sure all things you want to pause correctly use deltaTime...!
+            if (SceneManager.GetActiveScene().buildIndex == 1) {
+                hubButton.SetActive(false);
+            } else {
+                hubButton.SetActive(true);
+            }
+            
             pauseScene.SetActive(true);
             Time.timeScale = 0;
         }
--- Assets/Scripts/UI/PauseMenuButtonManager.cs
+++ Assets/Scripts/UI/PauseMenuButtonManager.cs
@@ -8,31 +8,13 @@ public class PauseMenuButtonManager : MonoBehaviour
 {
     //TODO: hide ToHubButton when pausing from hub; only show it when pausing from gameplay
 
-    // public int prevSceneIndex;
-
-    [SerializeField] public GameObject hubButton;
     [SerializeField] public GameObject pausePanel;
-
-    // get the index of the scene you paused from when pause scene is instantiated
-    // private void Start()
-    // {
-    //     prevSceneIndex = SceneManager.GetActiveScene().buildIndex;
-    //     Debug.Log("paused from scene " + prevSceneIndex);
-
-    //     Time.timeScale = 0;
-
-    //     // don't want to show BacktoHub if we paused from hub
-    //     if (prevSceneIndex == 1) { 
-    //         hubButton.SetActive(false);
-    //     } else {
-    //         hubButton.SetActive(true);
-    //     }
-    // }
 
     public void OnPauseMenuHubButtonPress()
     {
         SceneHandler.Instance.ToHubScene();
         Debug.Log("going to hub from pause screen");
+        Time.timeScale = 1;
     }
 
     public void OnPauseMenuQuitButtonPress()
@@ -43,21 +25,7 @@ public class PauseMenuButtonManager : MonoBehaviour
 
     public void OnPauseMenuResumeButtonPress()
     {
-        // not 100% sure what the pause implementation we want will be,
-        // but for now we'll just go with manipulating timeScale
         Time.timeScale = 1;
         pausePanel.SetActive(false);
-        // if (prevSceneIndex == 1)
-        // {
-        //     SceneHandler.Instance.ToHubScene();
-        // }
-        // else if (prevSceneIndex == 2)
-        // {
-        //     SceneHandler.Instance.ToGameplayScene();
-        // }
-        // else
-        // {
-        //     Debug.Log("Error: no scene to return to");
-        // }
     }
 }
