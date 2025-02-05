74048d94b4d369880bec8fe900bd722c9f0288eb
Dylan Paras <dkparas@ucsc.edu>
2025-02-04 19:39:46 -0800

    Added a rounding to the amount of health transferred
    added a math.round function to the amount of health transferred, seems to work fine

--- Assets/Scripts/Gatherer Health Transfer Ability/AbilityHealthTransfer.cs
+++ Assets/Scripts/Gatherer Health Transfer Ability/AbilityHealthTransfer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using Unity.Mathematics;
+using System;
+
 public class AbilityHealthTransfer : MonoBehaviour
 {
 
@@ -13,18 +16,18 @@ void Update() {
 
 
 //TODO: Create Timer for Ability Health Transfer.
-//TODO: Create Rounds up or down
+//TODO: Create Rounds up or down (Math.Round())
 
     if (Input.GetKeyDown(KeyCode.Q) && StatsManager.Instance.GathererCurrentHealth > healthgate) { //Ability confirmed to be Q && Timer
         // transfer health from Gatherer to Wanderer
 
       temp = StatsManager.Instance.GathererMaxHealth *  StatsManager.Instance.healthTransferAmount; // temp holds %25 percent of Gatherer's current health
 
-      StatsManager.Instance.GathererCurrentHealth -= temp; // Subtract from current gatherer health
+      StatsManager.Instance.GathererCurrentHealth -= math.round(temp); // Subtract from current gatherer health
 
       //
 
-        StatsManager.Instance.wandererHealth+= temp; 
+      StatsManager.Instance.wandererHealth += math.round(temp); // Adds to current Warden health
 
         if(StatsManager.Instance.wandererHealth > 10) { //above 10 health
             StatsManager.Instance.wandererHealth = 10;
@@ -32,7 +35,10 @@ void Update() {
             
             temp = 0f;
 
-    }
+            Debug.Log("Current Warden Health: " + StatsManager.Instance.wandererHealth);
+            Debug.Log("Current Gatherer Health: " + StatsManager.Instance.GathererCurrentHealth);
+
+        }
 }
 
 }
