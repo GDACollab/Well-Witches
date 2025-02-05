cdb0fd289d18455758f69770d52cacfc0dd92511
Alfredo <alfredinho303@gmail.com>
2025-02-04 19:11:01 -0800

    update from mac to pc
    Our branch we worked off from tuesday

--- Assets/Scripts/Gatherer Health Transfer Ability/AbilityHealthTransfer.cs
+++ Assets/Scripts/Gatherer Health Transfer Ability/AbilityHealthTransfer.cs
@@ -11,21 +11,25 @@ public class AbilityHealthTransfer : MonoBehaviour
 
 void Update() {
 
-    if (Input.GetKeyDown(KeyCode.Q) && StatsManager.Instance.GathererCurrentHealth > healthgate) { //Ability confirmed to be Q
+
+//TODO: Create Timer for Ability Health Transfer.
+//TODO: Create Rounds up or down
+
+    if (Input.GetKeyDown(KeyCode.Q) && StatsManager.Instance.GathererCurrentHealth > healthgate) { //Ability confirmed to be Q && Timer
         // transfer health from Gatherer to Wanderer
 
       temp = StatsManager.Instance.GathererMaxHealth *  StatsManager.Instance.healthTransferAmount; // temp holds %25 percent of Gatherer's current health
 
       StatsManager.Instance.GathererCurrentHealth -= temp; // Subtract from current gatherer health
-      
+
+      //
 
         StatsManager.Instance.wandererHealth+= temp; 
 
         if(StatsManager.Instance.wandererHealth > 10) { //above 10 health
             StatsManager.Instance.wandererHealth = 10;
         }
-            Debug.Log("wander health:" + StatsManager.Instance.wandererHealth);
-            Debug.Log("wander health:" + StatsManager.Instance.wandererHealth);
+            
             temp = 0f;
 
     }
