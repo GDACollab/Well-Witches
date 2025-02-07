40ff386a623ecf28bbadca539ea0493ce1277210
melikestuff <149332214+melikestuff@users.noreply.github.com>
2025-02-07 01:10:35 -0800

    im going to sleep gn

--- Assets/Scripts/Interaction/ItemDispenser.cs
+++ Assets/Scripts/Interaction/ItemDispenser.cs
@@ -7,25 +7,39 @@ public class ItemDispenser : MonoBehaviour, IInteractable
 
     [SerializeField]
     private Item[] items;
-    
-    /* // Start is called before the first frame update
+
+    private SpriteRenderer spriteRenderer;
+
+    private float timer = 0f;
+    [SerializeField] private float respawnTime = 30f;
+
+    // Start is called before the first frame update
     void Start()
     {
- 
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
+    /*
     // Update is called once per frame
     void Update()
     {
-        
-    } */
-
+        timer += Time.deltaTime;
+    }
+    */
     void Dispense()
     {
         Item itemToDispense = ChooseItem();
 
         Debug.Log($"Dispensing item: {itemToDispense}");
+        spriteRenderer.color = Color.grey;
+        StartCoroutine(TrackTime());
 
+    }
+
+    IEnumerator TrackTime()
+    {
+        // Wait for 30 seconds (Unity uses real-time seconds)
+        yield return new WaitForSeconds(respawnTime);
+        spriteRenderer.color = Color.green;
 
     }
 
