7daa562669332526b64d6e9567b06d97c4f68f93
Justin Lam <justinflam9225k@gmail.com>
2025-02-22 01:46:16 -0800

    Simplified movement code

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -23,24 +23,17 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
 	{
-		Move();
-	}
-
-	void Move()
-	{
+		// Get the direction we need to go in order to get to where we want to go (deltaVelocity)
 		Vector2 currentVelocity = rb.velocity;
 		Vector2 targetVelocity = moveDirection * maxSpeed_Adjusted;
 		Vector2 deltaVelocity = targetVelocity - currentVelocity;
 
 		float acceleration;
-		if (movementData.conserveMomentum && currentVelocity.magnitude > maxSpeed_Adjusted && Vector2.Dot(rb.velocity.normalized, targetVelocity.normalized) >= 0.5) 
-		{
-			acceleration = 0;
-        }
-		else acceleration = (moveDirection != Vector2.zero) ? movementData.acceleration : movementData.deceleration;
+		if (movementData.conserveMomentum && currentVelocity.magnitude > maxSpeed_Adjusted && Vector2.Angle(currentVelocity, targetVelocity) <= 60) acceleration = 0;
+		else if (moveDirection != Vector2.zero) acceleration = movementData.acceleration;
+		else acceleration = movementData.deceleration;
 
 		Vector2 accelerationVector = deltaVelocity * acceleration;
-
-		rb.AddForce(accelerationVector, ForceMode2D.Force);
+		rb.AddForce(accelerationVector);
 	}
 }
