5089888b930cf37306dceb04b46a777dbefa777a
Eric <ericlien749@gmail.com>
2025-02-21 19:49:33 -0800

    Added Key Items Dropping from Special Bushes
    Changed the Z value of the Special Bushes prefab from 0 to 1.

--- Assets/Scripts/Interaction/ItemDispenser.cs
+++ Assets/Scripts/Interaction/ItemDispenser.cs
@@ -11,8 +11,13 @@ public class ItemDispenser : MonoBehaviour, IInteractable
     private SpriteRenderer spriteRenderer;
     private ParticleSystem particleSystem;
 
+    private bool interacted = false;
+
     private float timer = 0f;
     [SerializeField] private float respawnTime = 30f;
+
+    [SerializeField] private GameObject prefabToSpawn;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,13 +34,23 @@ public class ItemDispenser : MonoBehaviour, IInteractable
     */
     void Dispense()
     {
-        Item itemToDispense = ChooseItem();
-
-        Debug.Log($"Dispensing item: {itemToDispense}");
-        spriteRenderer.color = Color.grey;
-        particleSystem.Stop();
-        StartCoroutine(TrackTime());
-
+        if (!interacted){
+            int spawnX = Random.Range(-1, 2);
+            int spawnY = 0;
+            if(spawnX == 0) {
+                spawnY = Random.Range(-1, 1) < 0 ? -1 : 1;
+            } else {
+                spawnY = Random.Range(-1, 2);
+            }
+            Item itemToDispense = ChooseItem();
+            Debug.Log($"Dispensing item: {itemToDispense}");
+            Instantiate(prefabToSpawn, new Vector3 (transform.position.x + spawnX,
+                                            transform.position.y +spawnY, 0), Quaternion.identity);
+            particleSystem.Stop();
+            spriteRenderer.color = Color.grey;
+            interacted = true;
+            StartCoroutine(TrackTime());
+        }
     }
 
     IEnumerator TrackTime()
