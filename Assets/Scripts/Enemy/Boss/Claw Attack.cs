0e1331f206c7d74486acea5b7907fae33578f1e4
Icicle333 <Liammccabe2006@outlook.com>
2025-02-21 19:25:15 -0800

    Update Claw Attack.cs

--- Assets/Scripts/Enemy/Boss/Claw Attack.cs
+++ Assets/Scripts/Enemy/Boss/Claw Attack.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ClawAttack : MonoBehaviour
 {
+    // These variables are used in the script to help with the attack and cooldown functions
     public float attackCooldown = 2f;
     public float attackTimer;
-    public float attackRange = 3f;
-    public float lookRadius = 15f;
+    public float dmg;
+    private bool canDmg;
+    public float attackDuration;
+    public float timeSinceAttack;
     public float targetHealth = 100;
     private float bleedingdamagepersecond = 0.5f; 
     
@@ -19,11 +22,26 @@ public class ClawAttack : MonoBehaviour
         Debug.Log("Bleeding");
         targetHealth = targetHealth - bleedingdamagepersecond;
     }
-    public void onTriggerEnter2D(Collider2D other){
-        IDamageable hit = other.GetComponent<IDamageable>();
-        if(hit != null){
-            hit.TakeDamage(clawDamage);
-            target.Bleeding().applyDamage();
+    void Update(){
+        timeSinceAttack += Time.deltaTime;
+        if (timeSinceAttack >= attackCooldown){
+            canDmg = false;
+        }
+    }
+    public void Attack(){
+        canDmg = true;
+        timeSinceAttack = 0;
+    }
+    public void StopAttack(){
+        canDmg = false;
+    }
+    void onTriggerEnter(Collider other){
+        if (other.gameObject.tag == "Player"){
+            target = other.transform;
+            if (canDmg){
+                target.GetComponent<PlayerHealth>().TakeDamage(clawDamage);
+                Bleeding();
+            }
         }
     }
 }
