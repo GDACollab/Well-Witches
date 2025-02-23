67d3fbc6ef1e39bbb1a57d551c596fa4e459e067
Derek Simpson <articunolies22@gmail.com>
2025-02-23 11:45:14 -0800

    Boss Setup

--- Assets/Scripts/Boss/Boss State Machine/Phases/PhaseOne.cs
+++ Assets/Scripts/Boss/Boss State Machine/Phases/PhaseOne.cs
@@ -6,7 +6,9 @@ public class PhaseOne : State
 {
     private Rigidbody2D rb;
     private StateMachine stateMachine;
+    private BossEnemy bossEnemy;
 
+    private bool useShieldBash = true; // Flag to alternate between attacks
 
 
     public PhaseOne(GameObject owner) : base(owner) { }
@@ -16,18 +18,49 @@ public class PhaseOne : State
         this.stateMachine = stateMachine;
         this.owner = owner;
         rb = owner.GetComponent<Rigidbody2D>();
-
+        bossEnemy = owner.GetComponent<BossEnemy>();
     }
 
     public override void OnEnter()
     {
-        Debug.Log("Entering Patrol State");
+        Debug.Log("Entering Phase One");
 
     }
 
     public override void OnUpdate()
     {
+        bossEnemy.TargetClosestPlayer();
+        if (bossEnemy.currentTarget != null)
+        {
+            if (bossEnemy.distanceToTarget >= bossEnemy.LungeDistance)
+            {
+                bossEnemy.LungeAttack();
+                Debug.Log("Lunge Attack");
+            }
+            else
+            {
+                // Move towards the player
+                Vector2 direction = (bossEnemy.currentTarget.position - bossEnemy.transform.position).normalized;
+                rb.MovePosition(rb.position + direction * bossEnemy.moveSpeed * Time.deltaTime);
+                Debug.Log("Moving towards player");
+            }
 
+            // Alternate between Shield_Bash and Sword_Slash when in range
+            if (bossEnemy.distanceToTarget <= bossEnemy.range)
+            {
+                if (useShieldBash)
+                {
+                    bossEnemy.Shield_Bash();
+
+                }
+                else
+                {
+                    bossEnemy.Sword_Slash();
+                }
+                // Toggle the flag for the next update
+                useShieldBash = !useShieldBash;
+            }
+        }
     }
 
     public override void OnExit()
