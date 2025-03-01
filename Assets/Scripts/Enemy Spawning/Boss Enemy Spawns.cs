6df9973bfd17eff023dbfe381b0409fca74f23fa
Icicle333 <Liammccabe2006@outlook.com>
2025-02-28 19:51:52 -0800

    WorkJam Night
    Debugged Claw Attack
    Made Boss Scene
    Tested Enemy Spawner For Boss

--- Assets/Scripts/Enemy Spawning/Boss Enemy Spawns.cs
+++ Assets/Scripts/Enemy Spawning/Boss Enemy Spawns.cs
@@ -6,16 +6,17 @@ public class BossEnemySpawns : EnemySpawner
 {
     
     EnemySpawner enemySpawner = new EnemySpawner();
-    private float timer = 0.0f;
+    [SerializeField] private float timer = 0.0f;
     public void phaseOne()
     {
         
         //Phase one should just be the normal spawning of enemies, we can make it more frequent but for now this spawn rate should be the same
-        timer = +Time.deltaTime;
+        timer += Time.deltaTime;
         if (timer > 10)
         {
             enemySpawner.SpawnSingle(goofyBoy, 3);
             enemySpawner.SpawnFormation(referencePoint.position, 45);
+            timer = 0;
         }
         
         
@@ -24,22 +25,32 @@ public class BossEnemySpawns : EnemySpawner
 
     public void phaseTwo()
     {
-        timer = +Time.deltaTime;
+        timer += Time.deltaTime;
         if (timer > 5)
         {
             enemySpawner.SpawnSingle(goofyBoy, 3);
             enemySpawner.SpawnFormation(referencePoint.position, 45);
+            timer = 0;
         }
     }
     // Update is called once per frame
     void Update()
     {
+        phaseOne();
         
         //This should spawn enemies form the enemy spawner class into the boss
-        enemySpawner.SpawnSingle(goofyBoy, 3);
+        //enemySpawner.SpawnSingle(goofyBoy, 3);
 
         //enemySpawner.SpawnSingle(goofyBoy, 3);
         //enemySpawner.SpawnSurrounded(goofyBoy, 3, 2);
         //enemySpawner.SpawnFormation(referencePoint.position, 45); 
+    }
+
+    void Start()
+    {
+        this.referencePoint = this.gameObject.transform;
+        enemySpawner.referencePoint = this.referencePoint;
+
+        enemySpawner.goofyBoy = this.goofyBoy;
     }
 }
