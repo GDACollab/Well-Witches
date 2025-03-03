269afb7ee2386bf28972a5108c462e862c90f5e6
Justin Lam <justinflam9225k@gmail.com>
2025-03-03 14:02:41 -0800

    Edited Player collision layer to make players unable to collide with each other

--- Assets/Scripts/Player_Controller/PlayerController_Gatherer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PlayerController_Gatherer : PlayerMovement
-{	
-	void OnCollisionEnter2D(Collision2D collision)
-    {
-		if (collision.gameObject.CompareTag("Player")) Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
-    }
-}
