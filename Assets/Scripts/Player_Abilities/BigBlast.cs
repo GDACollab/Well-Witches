4f01abadc08175f126fb66d256639313bc0bcb11
Justin Lam <justinflam9225k@gmail.com>
2025-03-03 16:18:40 -0800

    Implemented damage and knockback to big blast

--- Assets/Scripts/Player_Abilities/BigBlast.cs
+++ Assets/Scripts/Player_Abilities/BigBlast.cs
@@ -1,10 +1,26 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BigBlast : MonoBehaviour
 {
-	public void Activate(float lifespan)
+	float damagePerTick;
+	float damageTickDuration;
+	float damageTickCounter = 0;
+
+	float knockbackForce;
+	float knockbackTickDuration;
+	float knockbackTickCounter = 0;
+
+	HashSet<Collider2D> enemiesInBlast = new HashSet<Collider2D>();
+
+	public void Activate(float damagePerTick, float damageTickDuration, float knockbackForce, float knockbackTickDuration, float lifespan)
 	{
+		this.damagePerTick = damagePerTick;
+		this.damageTickDuration = damageTickDuration;
+		this.knockbackForce = knockbackForce;
+		this.knockbackTickDuration = knockbackTickDuration;
+
 		StartCoroutine(DeathTimer(lifespan));
 	}
 
@@ -16,8 +32,62 @@ public class BigBlast : MonoBehaviour
 
 	void Update()
 	{
+		AimAtMouse();
+		HandleDamageTick();
+		HandleKnockbackTick();
+	}
+
+	void AimAtMouse()
+	{
 		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector2 mouseDirection = (mousePosition - transform.position).normalized;
 		transform.right = mouseDirection;
+	}
+
+	void HandleDamageTick()
+	{
+		if (damageTickCounter > 0) damageTickCounter -= Time.deltaTime;
+		if (damageTickCounter <= 0)
+		{
+			DamageEnemies();
+			damageTickCounter = damageTickDuration;
+		}
+	}
+
+	void DamageEnemies()
+	{
+		foreach (Collider2D collider in enemiesInBlast) collider.GetComponent<BaseEnemyClass>().takingDamage((int)damagePerTick);
+	}
+
+	void HandleKnockbackTick()
+	{
+		if (knockbackTickCounter > 0) knockbackTickCounter -= Time.deltaTime;
+		if (knockbackTickCounter <= 0)
+		{
+			KnockbackEnemies();
+			knockbackTickCounter = knockbackTickDuration;
+		}
+	}
+
+	void KnockbackEnemies()
+	{
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 mouseDirection = (mousePosition - transform.position).normalized;
+
+		foreach (Collider2D collider in enemiesInBlast)
+		{
+			Debug.Log($"Knocking back {collider.name}");
+			collider.GetComponent<Rigidbody2D>().AddForce(mouseDirection * knockbackForce, ForceMode2D.Impulse);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.CompareTag("Enemy")) enemiesInBlast.Add(collider);
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		enemiesInBlast.Remove(collider);	// will removes if item in set
 	}
 }
--- Assets/Scripts/Player_Controller/Warden_BigBlast.cs
+++ Assets/Scripts/Player_Controller/Warden_BigBlast.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class Warden_BigBlast : MonoBehaviour
 {
-    [SerializeField] float duration;
+    [Header("Damage")]
+    [SerializeField] float damagePerTick;
+    [SerializeField, Tooltip("in seconds")] float damageTickDuration;
+
+    [Header("Knockback")]
+    [SerializeField] float knockbackForce;
+    [SerializeField, Tooltip("in seconds")] float knockbackTickDuration;
+
+    [Header("Duration")]
+    [SerializeField, Tooltip("in seconds")] float abilityDuration;
+
+    [Header("References")]
     [SerializeField] Transform spawnPoint;
     [SerializeField] GameObject prefab;
 
     void OnActivateAbility()    // called by the Player Input component
     {
         BigBlast bb = Instantiate(prefab, spawnPoint).GetComponent<BigBlast>();
-        bb.Activate(duration);
+        bb.Activate(damagePerTick, damageTickDuration, knockbackForce, knockbackTickDuration, abilityDuration);
     }
 }
