749b170a4fc2b22077e7cfbd494498ac2971ee94
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-03-06 14:10:33 -0800

    removed updating navmesh during runtime

--- Assets/Scripts/NavmeshScripts/UpdateNavmesh.cs
+++ Assets/Scripts/NavmeshScripts/UpdateNavmesh.cs
@@ -1,28 +1,15 @@
 using NavMeshPlus.Components;
-using System.Collections;
 using UnityEngine;
 
-// updates Navmesh surface every 3 seconds
 // TODO: should be changed to every time an obstacle spawns/despawns whenever that is implemented
 public class UpdateNavmesh : MonoBehaviour
 {
     [SerializeField] private NavMeshSurface navMeshSurface;
-    public bool updateMesh = true;
-    public float updateCooldown = 3f;
     
 
     private void Start()
     {
-        StartCoroutine(UpdateMesh());
-    }
-
-    private IEnumerator UpdateMesh()
-    {
-        while (updateMesh)
-        {
-            navMeshSurface.BuildNavMesh();
-            yield return new WaitForSeconds(updateCooldown);
-        }
+        navMeshSurface.BuildNavMesh();
     }
 
 }
