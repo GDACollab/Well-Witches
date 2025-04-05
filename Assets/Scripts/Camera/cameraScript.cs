9ee479bb9edb6d93b84697f5cfb7710f2b61c818
NinjaFromConsole <rothtyler24@gmail.com>
2025-04-04 19:46:37 -0700

    Camera Lerp
    - Smooth camera go brrrrrrrrrrrrrrrrrrrrrr

--- Assets/Scripts/Camera/cameraScript.cs
+++ Assets/Scripts/Camera/cameraScript.cs
@@ -12,21 +12,26 @@ public class cameraScript : MonoBehaviour
     [Tooltip("Miniumm coordinate the camera can move.")]
     [SerializeField] private Vector2 minViewCoord;
 
+    [Tooltip("How fast the camera lerps to the target.")]
+    [SerializeField] private float cameraMoveSpeed = 2f;
+
     private float x;
     private float y;
 
     private void Update()
     {
-        transform.position = target.transform.position;
+        //Move camera to target gameobject
+        //transform.position = target.transform.position;
 
-        x = transform.position.x;
+        //Clamp x and y positions based on min and max view coords
+        x = target.transform.position.x;
         x = Mathf.Min(x, maxViewCoord.x);
         x = Mathf.Max(x, minViewCoord.x);
 
-        y = transform.position.y;
+        y = target.transform.position.y;
         y = Mathf.Min(y, maxViewCoord.y);
         y = Mathf.Max(y, minViewCoord.y);
 
-        transform.position = new Vector3(x, y, -11);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, -11), Time.deltaTime * cameraMoveSpeed);
     }
 }
