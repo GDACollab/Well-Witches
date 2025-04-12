ee01e6b573aa111dc5d4513207f4900b38f902c7
NinjaFromConsole <rothtyler24@gmail.com>
2025-04-11 19:57:42 -0700

    Started Ability Selection UI
    - Added ability buttons, can select one  at a time from each group
    - Clicking the selected ability deselects it and causes you to select no abilities
    - Ability buttons have normal, hover, and selected sprites

---
+++ Assets/Scripts/UI/AbilitySelectIndividualAbilities.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AbilitySelectIndividualAbilities : MonoBehaviour, IPointerEnterHandler
+{
+
+    [SerializeField] private Image abilityImage;
+    [SerializeField] private Button abilityButton;
+    [SerializeField] private AbilitySelectManager abilitySelectManager;
+
+    [Header("Images")]
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite hoverSprite;
+    [SerializeField] private Sprite selectSprite;
+
+    [Header("Text")]
+    [SerializeField] private string abilityText;
+    [SerializeField] private string abilityName;
+
+    [Header("Ability Info")]
+    [SerializeField] private bool isWarden;
+    [SerializeField] private bool isActive;
+
+    private int id;
+
+    private void Start()
+    {
+        abilityImage.sprite = normalSprite;
+
+        SpriteState spriteState = new SpriteState();
+        spriteState.highlightedSprite = hoverSprite;
+        abilityButton.spriteState = spriteState;
+        abilityButton.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick()
+    {
+        abilitySelectManager.clickedAbility(isWarden, isActive, id);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        abilitySelectManager.updateHoveredAbility(id);
+    }
+
+    //So uhhhhh this code doesnt work for some reason :(
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        abilitySelectManager.updateHoveredAbility(-1);
+        Debug.Log("kljsfdavaksdvhasdv");
+    }
+
+
+    public void setSelected(bool selected)
+    {
+        if (selected)
+        {
+            abilityImage.sprite = selectSprite;
+        }
+        else
+        {
+            abilityImage.sprite = normalSprite;
+        }
+    }
+
+    public bool getIsWarden()
+    {
+        return isWarden;
+    }
+
+    public bool getIsActive()
+    {
+        return isActive;
+    }
+
+    public string getAbilityText()
+    {
+        return abilityText;
+    }
+
+    public string getAbilityName()
+    {
+        return abilityName;
+    }
+
+    //Run at the start of the game in abilityselectmanager
+    public void setID(int newID)
+    {
+        id = newID;
+    }
+}
---
+++ Assets/Scripts/UI/AbilitySelectManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AbilitySelectManager : MonoBehaviour
+{
+
+    [SerializeField] private int selectedPassiveAbilityWarden;
+    [SerializeField] private int selectedActiveAbilityWarden;
+
+    [SerializeField] private int selectedPassiveAbilityGatherer;
+    [SerializeField] private int selectedActiveAbilityGatherer;
+
+    [SerializeField] private AbilitySelectIndividualAbilities[] abilitiesList;
+
+    [SerializeField] private TextMeshProUGUI abilityInfoText;
+
+    [SerializeField] private string defaultAbilityText;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < abilitiesList.Length; i++)
+        {
+            abilitiesList[i].setID(i);
+        }
+
+        updateHoveredAbility(-1);
+    }
+
+    public void clickedAbility(bool warden, bool active, int id)
+    {
+        int selected = -1;
+        if (warden)
+        {
+            if (active)
+            {
+                selected = selectedActiveAbilityWarden;
+                selectedActiveAbilityWarden = selectAbilityDoStuff(selected, warden, active, id);
+            }
+            else
+            {
+                selected = selectedPassiveAbilityWarden;
+                selectedPassiveAbilityWarden = selectAbilityDoStuff(selected, warden, active, id);
+            }
+        }
+        else
+        {
+            if (active)
+            {
+                selected = selectedActiveAbilityGatherer;
+                selectedActiveAbilityGatherer = selectAbilityDoStuff(selected, warden, active, id);
+            }
+            else
+            {
+                selected = selectedPassiveAbilityGatherer;
+                selectedPassiveAbilityGatherer = selectAbilityDoStuff(selected, warden, active, id);
+            }
+        }
+    }
+
+    private int selectAbilityDoStuff(int selected, bool warden, bool active, int id) {
+        for (int i = 0; i < abilitiesList.Length; i++)
+        {
+            if (abilitiesList[i].getIsWarden() == warden && abilitiesList[i].getIsActive() == active)
+            {
+                abilitiesList[i].setSelected(false);
+            }
+        }
+
+        if (selected == id)
+        {
+            abilitiesList[id].setSelected(false);
+            selected = -1;
+        }
+        else
+        {
+            abilitiesList[id].setSelected(true);
+            selected = id;
+        }
+
+        return selected;
+    }
+
+    public void updateHoveredAbility(int id)
+    {
+        if (id == -1)
+        {
+            abilityInfoText.text = defaultAbilityText;
+            return;
+        }
+
+        abilityInfoText.text = abilitiesList[id].getAbilityText();
+    }
+
+}
