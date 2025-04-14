1d92eca86d08d06ea065ea93a27263a30e263066
Eric <ericlien749@gmail.com>
2025-04-14 16:00:18 -0700

    Just saving lol

---
+++ Assets/KeyItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class KeyItem : MonoBehaviour
+{
+    public int keyItemID;
+    public SpriteRenderer spriteRenderer;
+    private Sprite keyItemSprite; 
+    
+    public void setSprite(int ID) {
+        if (ID >= 1 || ID <= 9) {
+            keyItemSprite = Resources.Load<Sprite>($"Assets/Resources/KeyItems/KeyItemSprite {ID}.png");
+            spriteRenderer.sprite = keyItemSprite;
+        }
+    }
+
+}
