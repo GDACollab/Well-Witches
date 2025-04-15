513fe8a2d8b48eee370fdeb2df2fb495997d4edf
Mason <marobeck@ucsc.edu>
2025-04-14 21:38:01 -0700

    New call to detect bubble shield

--- Assets/Scripts/Enemy/RangedEnemy/EnemyProjectile.cs
+++ Assets/Scripts/Enemy/RangedEnemy/EnemyProjectile.cs
@@ -5,99 +5,129 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private float _lifetime;
-    private float _damage;
-    private float _AOESize;
-    private float _AOElifetime;
-    [HideInInspector] public float _AOEdamage;
-    [SerializeField] private GameObject[] projectileComponents;
-    [SerializeField] private GameObject AOEPrefab;
+	private Rigidbody2D rb;
+	private float _lifetime;
+	private float _damage;
+	private float _AOESize;
+	private float _AOElifetime;
+	[HideInInspector] public float _AOEdamage;
+	[SerializeField] private GameObject[] projectileComponents;
+	[SerializeField] private GameObject AOEPrefab;
 
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();   
-    }
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
 
-    // buncha math and stuff to make the projectile move towards the target and angled correctly
-    // I'll explain it if needed but eugh - Jim Lee
-    public void InitializeProjectile(Vector3 targetPosition, float offset, float projectileSpeed, float lifetime, float damage, float AOESize, float AOElifetime, float AOEDamage)
-    {
-        for (int i = 0; i < projectileComponents.Length; i++) 
-        {
-            projectileComponents[i].SetActive(true);
-        }
-        AOEPrefab.SetActive(false);
-        _lifetime = lifetime;
-        _damage = damage;
-        _AOESize = AOESize;
-        _AOElifetime = AOElifetime;
-        _AOEdamage = AOEDamage;
-        Vector3 direction = targetPosition - transform.position;
-        direction = Quaternion.Euler(0, 0, offset) * direction;
+	// buncha math and stuff to make the projectile move towards the target and angled correctly
+	// I'll explain it if needed but eugh - Jim Lee
+	public void InitializeProjectile(Vector3 targetPosition, float offset, float projectileSpeed, float lifetime, float damage, float AOESize, float AOElifetime, float AOEDamage)
+	{
+		for (int i = 0; i < projectileComponents.Length; i++)
+		{
+			projectileComponents[i].SetActive(true);
+		}
+		AOEPrefab.SetActive(false);
+		_lifetime = lifetime;
+		_damage = damage;
+		_AOESize = AOESize;
+		_AOElifetime = AOElifetime;
+		_AOEdamage = AOEDamage;
+		Vector3 direction = targetPosition - transform.position;
+		direction = Quaternion.Euler(0, 0, offset) * direction;
 
-        Vector3 rotation = transform.position - targetPosition;
-        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90 + offset);
+		Vector3 rotation = transform.position - targetPosition;
+		float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0, 0, rot + 90 + offset);
 
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * projectileSpeed;
-        StartCoroutine(DespawnProjectile());
-    }
+		rb.velocity = new Vector2(direction.x, direction.y).normalized * projectileSpeed;
+		StartCoroutine(DespawnProjectile());
+	}
 
-    // on hitting a Tag that isn't enemy
-    // instatiates(from pool) the AOE prefab and deactivates the projectile
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // if it collides with something that isn't tagged with Enemy spawn AOE
-        if (!collision.gameObject.CompareTag("Enemy"))
-        {
-            AOEPrefab.SetActive(true);
-            AOEPrefab.transform.localScale = Vector3.one * _AOESize;
-            rb.velocity = Vector3.zero;
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                //Debug.Log(collision.gameObject.name);
-                // TODO: DEAL DAMAGE TO PLAYER
-                // - Producer (ben) added some temp checks and calls to StatsManager to do damage here
 
-                if (collision.gameObject.name == "Gatherer")
-                {
-                    EventManager.instance.playerEvents.PlayerDamage(_damage, "Gatherer");
-                }
-                else if (collision.gameObject.name == "Warden")
-                {
-                    EventManager.instance.playerEvents.PlayerDamage(_damage, "Warden");
+	// on hitting a Tag that isn't enemy
+	// instatiates(from pool) the AOE prefab and deactivates the projectile
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		// if it collides with something that isn't tagged with Enemy spawn AOE
+		if (!collision.gameObject.CompareTag("Enemy"))
+		{
+			AOEPrefab.SetActive(true);
+			AOEPrefab.transform.localScale = Vector3.one * _AOESize;
+			rb.velocity = Vector3.zero;
+			if (!collision.gameObject.CompareTag("Enemy"))
+			{
+				if (collision.gameObject.CompareTag("Player"))
+				{
+					//Debug.Log(collision.gameObject.name);
+					// TODO: DEAL DAMAGE TO PLAYER
+					// - Producer (ben) added some temp checks and calls to StatsManager to do damage here
 
-                }
-            }
-            for (int i = 0; i < projectileComponents.Length; i++)
-            {
-                projectileComponents[i].SetActive(false);
-            }
-            StartCoroutine(DespawnAOE());
-        } 
-    }
+					if (collision.gameObject.name == "Gatherer")
+					{
+						EventManager.instance.playerEvents.PlayerDamage(_damage, "Gatherer");
+					}
+					else if (collision.gameObject.name == "Warden")
+					{
+						EventManager.instance.playerEvents.PlayerDamage(_damage, "Warden");
 
-    // used to expire the projectile if it doesn't hit anything
-    IEnumerator DespawnProjectile()
-    {
-        // spawns AOE prefab AFTER the main projectile expires
-        yield return new WaitForSeconds(_lifetime);
-        AOEPrefab.SetActive(true);
-        AOEPrefab.transform.localScale = Vector3.one * _AOESize;
-        rb.velocity = Vector3.zero;
-        for (int i = 0; i < projectileComponents.Length; i++)
-        {
-            projectileComponents[i].SetActive(false);
-        }
-        StartCoroutine(DespawnAOE());
-    }
+					}
+				}
+			}
 
-    IEnumerator DespawnAOE()
-    {
-        yield return new WaitForSeconds(_AOElifetime);
-        AOEPrefab.SetActive(false);
-        gameObject.SetActive(false);
-    }
+			// Clean up gameobject
+			for (int i = 0; i < projectileComponents.Length; i++)
+			{
+				projectileComponents[i].SetActive(false);
+			}
+			StartCoroutine(DespawnAOE());
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (!collision.gameObject.CompareTag("Enemy"))
+		{
+			if (collision.gameObject.CompareTag("Player"))
+			{
+				//Debug.Log(collision.gameObject.name);
+				// TODO: DEAL DAMAGE TO PLAYER
+				// - Producer (ben) added some temp checks and calls to StatsManager to do damage here
+
+				if (collision.gameObject.name == "Gatherer")
+				{
+					BubbleShield shield = collision.gameObject.GetComponent<BubbleShield>();
+					if (shield.isShieldActive)
+					{
+						//Debug.Log("Projectile deflected!");
+						rb.velocity = -rb.velocity; // Bounce off
+						transform.Rotate(0, 0, 180);    // Rotate the sprite
+					}
+				}
+			}
+		}
+	}
+
+	// used to expire the projectile if it doesn't hit anything
+	IEnumerator DespawnProjectile()
+	{
+		// spawns AOE prefab AFTER the main projectile expires
+		yield return new WaitForSeconds(_lifetime);
+		AOEPrefab.SetActive(true);
+		AOEPrefab.transform.localScale = Vector3.one * _AOESize;
+		rb.velocity = Vector3.zero;
+		for (int i = 0; i < projectileComponents.Length; i++)
+		{
+			projectileComponents[i].SetActive(false);
+		}
+		StartCoroutine(DespawnAOE());
+	}
+
+	IEnumerator DespawnAOE()
+	{
+		yield return new WaitForSeconds(_AOElifetime);
+		AOEPrefab.SetActive(false);
+		gameObject.SetActive(false);
+	}
 }
