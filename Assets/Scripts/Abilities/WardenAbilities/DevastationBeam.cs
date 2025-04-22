6839434f94cef06909127cabd7dc282d997688bf
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-04-22 00:02:08 -0700

    added impact frame to beam

--- Assets/Scripts/Abilities/WardenAbilities/DevastationBeam.cs
+++ Assets/Scripts/Abilities/WardenAbilities/DevastationBeam.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class DevastationBeam : MonoBehaviour
 {
@@ -20,7 +18,7 @@ public class DevastationBeam : MonoBehaviour
 
     public GameObject spellCircle;
     public GameObject laserBeam;
-    //public CameraShake cameraShake;
+    public CameraShake cameraShake;
     public GameObject volume;
     public GameObject light2d;
 
@@ -33,30 +31,34 @@ public class DevastationBeam : MonoBehaviour
 
     private void Start()
     {
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        mousePosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 rotation = mousePosition - transform.position;
-        float targetRotation = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, targetRotation);
+
 
 
     }
 
     public void Activate(float damagePerTick, float damageTickDuration, float knockbackForce, float knockbackTickDuration, float lifespan)
 	{
-		this.damagePerTick = damagePerTick;
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        mousePosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePosition - transform.position;
+        float targetRotation = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, targetRotation);
+        cameraShake = mainCam.GetComponentInParent<CameraShake>();
+
+        this.damagePerTick = damagePerTick;
 		this.damageTickDuration = damageTickDuration;
 		this.knockbackForce = knockbackForce;
 		this.knockbackTickDuration = knockbackTickDuration;
 
         spellCircle.SetActive(true);
-        laserBeam.SetActive(false);
+		laserBeam.SetActive(false);
         StartCoroutine(ActivateLaser(lifespan));
 	}
 
     IEnumerator ActivateLaser(float lifespan)
     {
         yield return new WaitForSeconds(1f);
+        if (cameraShake != null) { StartCoroutine(cameraShake.Shake(.14f, 0.4f, 0, volume, light2d)); }
         laserBeam.SetActive(true);
         StartCoroutine(DisableUltimate(lifespan));
     }
