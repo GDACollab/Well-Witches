52c43c2d80eaec13104fabaac285fec7e1c11ee9
Icicle333 <Liammccabe2006@outlook.com>
2025-04-25 19:54:53 -0700

    Dallahn Quest 2
    Updated work on Dallahn Quest, still work in progress but progressing smoothly, at least on my end

--- Assets/Scripts/Event System/Events/PumpkinHeadTimer.cs
+++ Assets/Scripts/Event System/Events/PumpkinHeadTimer.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PumpkinHeadTimer : MonoBehaviour
+public class PumpkinHeadTimer : PumpkinHead
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // This script is used to manage a timer for when Gallahn Head is picked up
+    // If player attacked three times after collecting said head, pumpkin destroyed, they have to find another
+    // :(
+    private float timer = 0f; // Timer to track time since pumpkin head was collected
+    private bool isPumpkinCollected = false; // Flag to check if pumpkin head is collected
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void pumpkinTimer(){
+        if (isPumpkinCollected)
+        {
+            timer += Time.deltaTime; // Increment timer
+            if (timer == 60) // Check if 60 seconds have passed
+            {
+               AnnouncementManager.Instance.AddAnnouncementToQueue("Gallahn Head have been safely recovered, head on over to Gallahn and return his head next time you see him, he won't!");
+            }
+        }
     }
+    
 }
