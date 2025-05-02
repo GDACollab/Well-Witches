0230f8c5cc41f8d80ab1414ff8b504ebd603e9e2
Icicle333 <Liammccabe2006@outlook.com>
2025-05-02 12:03:42 -0700

    Collection
    Code to see if pumpkin head can be collected

--- Assets/PumpkinHead.cs
+++ Assets/PumpkinHead.cs
@@ -10,7 +10,7 @@ public class PumpkinHead : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             EventManager.instance.miscEvent.PumpkinCollected();
-            
+            PumpkinHead.Destroy(gameObject); // Destroy the pumpkin head immediately
             
         }
     }
