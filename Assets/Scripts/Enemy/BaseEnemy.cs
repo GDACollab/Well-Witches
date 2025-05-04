f6b873fdf96916a23973355309a78b73fcbc6cf3
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-05-03 19:18:40 -0700

    removed the spaghetti in spellburst.cs

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -49,7 +49,7 @@ public class BaseEnemyClass : MonoBehaviour
     {
         yield return new WaitForSeconds(0.2f);
         agent.enabled = true;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.3f);
         rb.velocity = Vector3.zero;
     }
 
