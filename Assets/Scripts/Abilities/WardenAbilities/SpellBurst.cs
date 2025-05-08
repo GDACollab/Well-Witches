7b7835043d6e55351db86c69fa5471b6c6eeeb54
Thryloz <86812547+Thryloz@users.noreply.github.com>
2025-05-07 19:04:10 -0700

    IT DONE

--- Assets/Scripts/Abilities/WardenAbilities/SpellBurst.cs
+++ Assets/Scripts/Abilities/WardenAbilities/SpellBurst.cs
@@ -21,8 +21,8 @@ public class SpellBurst : MonoBehaviour
     private void Update()
     {
         timer += Time.deltaTime;
-        transform.position = parent.position;
-        if (transform.childCount == 0 && timer >= abilityDuration)
+        transform.position = new Vector3(parent.position.x, parent.position.y + 0.5f, parent.position.z);
+        if (transform.childCount == 1 && timer >= abilityDuration)
         {
             Destroy(gameObject);
         }
@@ -30,11 +30,13 @@ public class SpellBurst : MonoBehaviour
 
     IEnumerator SpawnSpectralProjectile(float damage, float speed, float rotationForce, float lifetime, float timeBetweenProjectile, int projectilePerBurst)
     {
-        while (true)
+        yield return new WaitForSeconds(0.5f);
+        while (timer <= abilityDuration)
         {
-            for (int i = 1; i <= projectilePerBurst; i++)
+            for (int i = 0; i < projectilePerBurst; i++)
             {
-                Quaternion rotation = transform.rotation * Quaternion.Euler(0, 0, 360/i);
+                Quaternion rotation = transform.rotation * Quaternion.Euler(0, 0, (360/projectilePerBurst) * i);
+                Debug.Log(rotation.eulerAngles);
                 SpellBurstProjectile projectile = Instantiate(projectilePrefab, transform.position, rotation).GetComponent<SpellBurstProjectile>();
                 projectile.Initialize(transform, damage, speed, rotationForce, lifetime);
             }
