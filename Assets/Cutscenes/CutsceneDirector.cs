f7e3063e15c77b3c4f17fd509c43bb37c05e4528
Jonah Ryan <jonahrobot@gmail.com>
2025-05-11 00:55:43 -0700

    Add support for click to continue

---
+++ Assets/Cutscenes/CutsceneDirector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneDirector : MonoBehaviour
+{
+    PlayableDirector currentDirector;
+
+    private void Start() {
+        currentDirector = gameObject.GetComponent<PlayableDirector>();
+    }
+
+    public void WaitForInput() {
+        if (currentDirector == null) {
+            Debug.LogError("Using CutsceneDirector.cs without a timeline on the same object!");
+        } else {
+            currentDirector.Pause();
+        }
+    }
+
+    public void AcceptInput() {
+        if (currentDirector == null) {
+            Debug.LogError("Using CutsceneDirector.cs without a timeline on the same object!");
+        } else {
+            currentDirector.Resume();
+        }
+    }
+}
