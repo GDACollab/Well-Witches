2cf7de2f5712e57ac14ff0b9117ee47f6f193679
Icicle333 <Liammccabe2006@outlook.com>
2025-05-18 23:11:32 -0700

    Added Scene Transition sending Player from Main Game back to Hub Scene
    Once players reached teh conditional for the Dullahn Side quest, at the end of the 60 second timer, it will send the player back to the hub scene to finish the quest.

--- Assets/Resources/Quests/Dallahn Quest/CollectPumpkinTimer.cs
+++ Assets/Resources/Quests/Dallahn Quest/CollectPumpkinTimer.cs
@@ -9,59 +9,61 @@ public class CollectPumpkinTimer : QuestStep
 {
     private float time = 0f;
     private int hits = 0;
-    private PlayerHealth playerHealth;
+   
     [SerializeField] public int timeToLive = 60;
     [SerializeField] public int hitsToLive = 3;
+    protected StatsManager statsManager;
     //[SerializeField] public GameObject questItem;
-    
-   
-   
-    
+
+
+
+
     private void Start()
     {
         //GameManager.instance.activeQuestPrefab = questItem;
         //GameManager.instance.activeQuestItemCount = garlicToCollect;
         questDescription.color = Color.white;
         questDescription.text = $"- Don't get hit (0/{hitsToLive}) times within the next (0/{timeToLive}) seconds";
-        PumpkinTimerQuest();
+
     }
 
-    private void Update()
+    void Update()
     {
+        
         time += Time.deltaTime; // increment the timer
-        questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({time}/{timeToLive}) seconds";
-        if (time >= timeToLive) // if timer hits 0 without fail
+        int timeForDisplay = (int)(time % 60); // get the time for display
+        questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({timeForDisplay}/{timeToLive}) seconds";
+        if (time >= timeToLive) // if timer hits without canceling the quest
         {
 
             questDescription.color = Color.green;
             questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next (60/60) seconds";
             FinishQuestStep();
-            
+            SceneHandler.Instance.ToHubScene();
+
         }
+        
+        
+        
+
     }
-    private void PumpkinTimerQuest()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-        if (playerHealth == null)
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            Debug.LogError("PlayerHealth component not found on Gatherer object.");
-            return;
+
+
+            hits += 1;
+            questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({(int)(time % 60)}/{timeToLive}) seconds";
+
         }
-       
-        
-       
-        
-        
-        /*if (hits >= hitsToLive) // if timer hits 0 wihtout fail
+        if (hits >= hitsToLive)
         {
             questDescription.color = Color.red;
-             questDescription.text = $"- You failed to save the Pumpkin, you must start over and find a new one.";
-            // find the cancel function
-            CollectPumpkinQuest(); // cancel the quest step
+            questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next (60/60) seconds";
+            CancelQuestStep();
         }
-        //*/
-        
-       
-        
     }
-}
+    
+}  
