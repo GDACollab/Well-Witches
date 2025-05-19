1985856f7af28b342b726d462ff2af878ad7a38e
Icicle333 <Liammccabe2006@outlook.com>
2025-05-19 14:50:41 -0700

    Hit Counter
    The counter for detecting damage done onto the Gatherer is done.

--- Assets/Resources/Quests/Dallahn Quest/CollectPumpkinTimer.cs
+++ Assets/Resources/Quests/Dallahn Quest/CollectPumpkinTimer.cs
@@ -10,20 +10,25 @@ public class CollectPumpkinTimer : QuestStep
     private float time = 0f;
     private int hits = 0;
     private StatsManager statsManager;
-    private float gathererHealth = 0.0f;
-    [SerializeField] public int timeToLive = 60;
-    [SerializeField] public int hitsToLive = 3;
-    
+    private float gathererHealth = 0f;
+
+
+    [SerializeField] private int timeToLive = 60;
+    [SerializeField] private int hitsToLive = 4;
+
     //[SerializeField] public GameObject questItem;
+
 
 
 
 
     private void Start()
     {
-        gathererHealth = statsManager.GathererCurrentHealth;
-        //GameManager.instance.activeQuestPrefab = questItem;
-        //GameManager.instance.activeQuestItemCount = garlicToCollect;
+        int hits = 0;
+        float time = 0f;
+        statsManager = StatsManager.Instance;
+        var gathererHealth = statsManager.GathererCurrentHealth;
+        print(gathererHealth);
         questDescription.color = Color.white;
         questDescription.text = $"- Don't get hit (0/{hitsToLive}) times within the next (0/{timeToLive}) seconds";
 
@@ -33,46 +38,49 @@ public class CollectPumpkinTimer : QuestStep
     {
 
         time += Time.deltaTime; // increment the timer
-        int timeForDisplay = (int)(time % 60); // get the time for display
-        questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({timeForDisplay}/{timeToLive}) seconds";
+        
+        questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({(int)(time % 60)}/{timeToLive}) seconds";
+        questDescription.color = Color.white;
+        
+        
+        PumpkinTimerChecker();
+        
+        
+
+    }
+    private void PumpkinTimerChecker()
+    {
         float check = statsManager.GathererCurrentHealth;
-        if (time >= timeToLive) // if timer hits without canceling the quest
+        print(check);
+        if ((time > timeToLive)) // if timer hits without canceling the quest
         {
 
-            questDescription.color = Color.green;
             questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next (60/60) seconds";
+            questDescription.color = Color.green;
             FinishQuestStep();
             SceneHandler.Instance.ToHubScene();
 
-        }
 
+        }
         if (hits >= hitsToLive) // if player gets hit
         {
-            questDescription.color = Color.red;
+            
             questDescription.text = $"You failed, now you must restart the quest";
+            questDescription.color = Color.red;
             CancelQuestStep();
             SceneHandler.Instance.ToHubScene();
         }
         // Want to check to see if the player is first the gatherer, then detect if the player was hit by an enemy by seeing if it had taken damage. 
-        if (gathererHealth > check)
-        {
-
+        if (gathererHealth != check) 
+      {
             gathererHealth = statsManager.GathererCurrentHealth;
-            hits++;    
-            questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({timeForDisplay}/{timeToLive}) seconds";
+            hits++;
+            questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({(int)(time % 60)}/{timeToLive}) seconds";
 
-            
+
         }
-            
-            
-
-            
-             
         
-        
-        
-        
-
+       
     }
 
     
