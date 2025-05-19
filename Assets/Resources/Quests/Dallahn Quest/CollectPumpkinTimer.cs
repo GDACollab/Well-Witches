9c9260860e41544f0db451cf76ffca0ec2d1d512
Icicle333 <Liammccabe2006@outlook.com>
2025-05-18 23:39:31 -0700

    Committing to my branch with what I have done
    Its not perfect or done but its close where if someone else wants to work on debugging, they can, doesn't present errors but after adding the damage detection, now its saying the task is complete.

--- Assets/Resources/Quests/Dallahn Quest/CollectPumpkinTimer.cs
+++ Assets/Resources/Quests/Dallahn Quest/CollectPumpkinTimer.cs
@@ -9,10 +9,11 @@ public class CollectPumpkinTimer : QuestStep
 {
     private float time = 0f;
     private int hits = 0;
-   
+    private StatsManager statsManager;
+    private float gathererHealth = 0.0f;
     [SerializeField] public int timeToLive = 60;
     [SerializeField] public int hitsToLive = 3;
-    protected StatsManager statsManager;
+    
     //[SerializeField] public GameObject questItem;
 
 
@@ -20,6 +21,7 @@ public class CollectPumpkinTimer : QuestStep
 
     private void Start()
     {
+        gathererHealth = statsManager.GathererCurrentHealth;
         //GameManager.instance.activeQuestPrefab = questItem;
         //GameManager.instance.activeQuestItemCount = garlicToCollect;
         questDescription.color = Color.white;
@@ -29,10 +31,11 @@ public class CollectPumpkinTimer : QuestStep
 
     void Update()
     {
-        
+
         time += Time.deltaTime; // increment the timer
         int timeForDisplay = (int)(time % 60); // get the time for display
         questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({timeForDisplay}/{timeToLive}) seconds";
+        float check = statsManager.GathererCurrentHealth;
         if (time >= timeToLive) // if timer hits without canceling the quest
         {
 
@@ -42,28 +45,36 @@ public class CollectPumpkinTimer : QuestStep
             SceneHandler.Instance.ToHubScene();
 
         }
-        
-        
-        
 
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-
-
-            hits += 1;
-            questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({(int)(time % 60)}/{timeToLive}) seconds";
-
-        }
-        if (hits >= hitsToLive)
+        if (hits >= hitsToLive) // if player gets hit
         {
             questDescription.color = Color.red;
-            questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next (60/60) seconds";
+            questDescription.text = $"You failed, now you must restart the quest";
             CancelQuestStep();
+            SceneHandler.Instance.ToHubScene();
         }
+        // Want to check to see if the player is first the gatherer, then detect if the player was hit by an enemy by seeing if it had taken damage. 
+        if (gathererHealth > check)
+        {
+
+            gathererHealth = statsManager.GathererCurrentHealth;
+            hits++;    
+            questDescription.text = $"- Don't get hit ({hits}/{hitsToLive}) times within the next ({timeForDisplay}/{timeToLive}) seconds";
+
+            
+        }
+            
+            
+
+            
+             
+        
+        
+        
+        
+
     }
+
+    
     
 }  
