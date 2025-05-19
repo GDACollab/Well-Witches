4cb128d6dd9e6ce347df2b70922391256c041c99
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-05-19 14:13:52 -0700

    got warden icons working

--- Assets/Scripts/UI/WardenIconChanger.cs
+++ Assets/Scripts/UI/WardenIconChanger.cs
@@ -18,11 +18,39 @@ public class WardenIconChanger : MonoBehaviour
     {
         string Active = WardenAbilityManager.Instance.GetEquippedActiveName();
         string Passive = WardenAbilityManager.Instance.GetEquippedPassiveName();
+
         switch (Active)
         {
             case "DevastationBeam":
                 ActiveIcon.GetComponent<RawImage>().texture = Icons[3];
-                ActiveIcon.GetComponent<RectTransform>().sizeDelta = Icons[3].Size() / 2;
+                ActiveIcon.GetComponent<RectTransform>().sizeDelta = Icons[3].Size() / 3;
+                break;
+            case "GourdForge":
+                ActiveIcon.GetComponent<RawImage>().texture = Icons[4];
+                ActiveIcon.GetComponent<RectTransform>().sizeDelta = Icons[4].Size() / 3;
+                break;
+            case "SpellBurst":
+                ActiveIcon.GetComponent<RawImage>().texture = Icons[5];
+                ActiveIcon.GetComponent<RectTransform>().sizeDelta = Icons[5].Size() / 3;
+                break;
+        }
+
+        switch (Passive)
+        {
+            case "ResurrectionRegalia":
+                PassiveIcon.GetComponent<RawImage>().texture = Icons[0];
+                PassiveIcon.GetComponent<RectTransform>().sizeDelta = Icons[0].Size() / 3;
+                break;
+            case "SoulSiphon":
+                PassiveIcon.GetComponent<RawImage>().texture = Icons[1];
+                PassiveIcon.GetComponent<RectTransform>().sizeDelta = Icons[1].Size() / 3;
+                break;
+            case "BoggyBullets":
+                PassiveIcon.GetComponent<RawImage>().texture = Icons[2];
+                PassiveIcon.GetComponent<RectTransform>().sizeDelta = Icons[2].Size() / 3;
+                break;
+            default:
+                PassiveIcon.GetComponent<RawImage>().texture = Icons[6];
                 break;
         }
     }
