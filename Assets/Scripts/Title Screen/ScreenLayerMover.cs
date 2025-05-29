1ecf70c43e892bc09939bfba9b164b1a465f4e75
AshtonTankGallistel <ashtontankgallistel@gmail.com>
2025-05-28 19:01:16 -0700

    Fixed a bug where the moving layers could reveal their edges on certain screen sizes. (Not perfect as extremely wide screens can still break the game, but that's true for all layers)

--- Assets/Scripts/Title Screen/ScreenLayerMover.cs
+++ Assets/Scripts/Title Screen/ScreenLayerMover.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ScreenLayerMover : MonoBehaviour
 {
@@ -24,17 +25,21 @@ public class ScreenLayerMover : MonoBehaviour
     {
         //Debug.Log("mousePos:" + Input.mousePosition);
         Vector3 mouseInfo = MouseBasedOnCenter();
-        layer1.transform.position = layer1BasePosition - mouseInfo * layer1MoveStrength;
-        layer2.transform.position = layer2BasePosition - mouseInfo * layer2MoveStrength;
+        Vector3 L1pos = layer1BasePosition - mouseInfo * layer1MoveStrength;
+        //clamp the position so that it can't go too far and revea the edges
+        Vector3 L1pos2 = new Vector3(Mathf.Clamp(L1pos.x, -layer1.rectTransform.rect.width * 0.05f, layer1.rectTransform.rect.width * 0.05f),
+            Mathf.Clamp(L1pos.y, -layer1.rectTransform.rect.height * 0.05f, layer1.rectTransform.rect.height * 0.05f));
+        layer1.rectTransform.localPosition = L1pos2;
+        layer2.rectTransform.localPosition = layer2BasePosition - mouseInfo * layer2MoveStrength;
     }
 
     //Mouse isn't properly centered, this helper funcion corrects the offset
     //NOTE TO SELF; correct when I have the chance! Should Ideally autocorrect based on the camera and/or image size
     Vector3 MouseBasedOnCenter()
     {
-        Vector3 myMousePos = new Vector3(Mathf.Clamp(Input.mousePosition.x, 0, layer1.rectTransform.rect.width / 2),
-            Mathf.Clamp(Input.mousePosition.y, 0, layer1.rectTransform.rect.height / 2));
-        Vector3 myInfo = myMousePos - new Vector3(layer1.rectTransform.rect.width / 4, layer1.rectTransform.rect.height / 4);
+        Vector3 myMousePos = new Vector3(Mathf.Clamp(Input.mousePosition.x, 0, Screen.width),
+            Mathf.Clamp(Input.mousePosition.y, 0, Screen.height));
+        Vector3 myInfo = myMousePos - new Vector3(Screen.width / 2, Screen.height / 2);
         //myInfo = new Vector3(clamp)
         return myInfo;
     }
