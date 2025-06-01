18261cc7c91b956c5ec5332045d0bb4fc335a309
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-06-01 02:22:35 -0700

    added ranged traversal sound

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -29,6 +29,7 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference bruiserAttackSwipe  { get; private set; }
     [field: SerializeField] public EventReference bruiserAttackDash  { get; private set; }
     [field: SerializeField] public EventReference rangedAttackFire  { get; private set; }
+    [field: SerializeField] public EventReference rangedTraversal  { get; private set; }
     [field: SerializeField] public EventReference rangedAttackHit  { get; private set; }
     [field: SerializeField] public EventReference bossLunge { get; private set; }
     [field: SerializeField] public EventReference bossPhase1Attack1  { get; private set; }
