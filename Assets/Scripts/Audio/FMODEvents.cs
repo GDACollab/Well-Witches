061ff91efe495210daf169956f06a847b2437d3c
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-06-01 02:03:49 -0700

    added boss phase 1 attack 2 sound

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -24,13 +24,14 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference gathererHurt{ get; private set; }
     [field: SerializeField] public EventReference wardenDown { get; private set; }
     [field: SerializeField] public EventReference wardenHurt{ get; private set; }
-    [field: SerializeField] public EventReference bossLunge { get; private set; }
     [field: SerializeField] public EventReference tankAttackBash { get; private set; }
     [field: SerializeField] public EventReference tankTraverse { get; private set; }
     [field: SerializeField] public EventReference bruiserAttackSwipe  { get; private set; }
     [field: SerializeField] public EventReference rangedAttackFire  { get; private set; }
     [field: SerializeField] public EventReference rangedAttackHit  { get; private set; }
+    [field: SerializeField] public EventReference bossLunge { get; private set; }
     [field: SerializeField] public EventReference bossPhase1Attack1  { get; private set; }
+    [field: SerializeField] public EventReference bossPhase1Attack2  { get; private set; }
     [field: SerializeField] public EventReference itemPickUp  { get; private set; }
     [field: SerializeField] public EventReference aloeTalk  { get; private set; }
     [field: SerializeField] public EventReference vervainTalk  { get; private set; }
