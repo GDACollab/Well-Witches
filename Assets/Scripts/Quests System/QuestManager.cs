9e8f7b566f5fc79924650194fa81cb06f1b6937e
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-06-05 20:50:16 -0700

    fixed namespace bugs that stopped build

--- Assets/Scripts/Quests System/QuestManager.cs
+++ Assets/Scripts/Quests System/QuestManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.Playables;
+//using UnityEditor.Playables;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
